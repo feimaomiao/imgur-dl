a0f880d372d3d84999252351712a27351bb7b959
feimaomiao <lcpmatthew@gmail.com>
2020-04-04 02:44:30 -0700

    New functions for formatting is introduced New formatting Calling new function from download.py, [dl]

--- __main__.py
+++ __main__.py
@@ -1,14 +1,17 @@
 from source import *
 
+
 # OSError
 # image.convert('RGB').save()
-
 def main():
-	args = treat_args(get_args())
-	if args['verbose']:
-		printarg(args)
-	link_generator = link_grabber(args['imgur_link'], args['verbose'])
-	link_generator.return_file_objects()
+    args = treat_args(get_args())
+    if args['verbose']:
+        printarg(args)
+    link_generator = link_grabber(args['imgur_link'], args['verbose'])
+    file_list = link_generator.return_file_objects()
+    dl(file_list, args['verbose'], args['output'], args['rename_each'],
+       args['format'])
+
 
 if __name__ == '__main__':
-	main()
+    main()
